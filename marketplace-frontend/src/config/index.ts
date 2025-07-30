360198b064669c83f07671cf38f105fdbfcaa4a1
ri-dev-git <rishi9850@outlook.com>
2025-07-30 18:54:34 +0530

    fix env names

--- marketplace-frontend/src/config/index.ts
+++ marketplace-frontend/src/config/index.ts
@@ -2,12 +2,7 @@ import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
 
-// Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID // this is a public projectId only to use on localhost
 
-if (!projectId) {
-    throw new Error('Project ID is not defined')
-}
 
 export const networks = [solanaDevnet] as [AppKitNetwork, ...AppKitNetwork[]]
 
